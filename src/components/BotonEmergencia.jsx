5701de5366d4b428e80cb15f3adc9e4d78db9c08
l2202 <L22650271@zitacuaro.tecnm.mx>
2026-05-06 13:57:35 -0600

    quitar comentario

--- src/components/BotonEmergencia.jsx
+++ src/components/BotonEmergencia.jsx
@@ -2,6 +2,6 @@ export default function BotonEmergencia() {
   const numeroEmergencia = "911";
   return (
     <a href={`tel:${numeroEmergencia}`} className="emergencia">📞</a>
-    //prueba
+
   );
 }
